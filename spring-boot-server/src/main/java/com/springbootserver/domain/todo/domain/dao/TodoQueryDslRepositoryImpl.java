9449f356b0763a99659d8e2289df8f47a5c85917
solchan98 <dev.sol0427@gmail.com>
2022-02-11 21:36:46 +0900

    refactor: 조회 조건 추가

--- spring-boot-server/src/main/java/com/springbootserver/domain/todo/domain/dao/TodoQueryDslRepositoryImpl.java
+++ spring-boot-server/src/main/java/com/springbootserver/domain/todo/domain/dao/TodoQueryDslRepositoryImpl.java
@@ -4,6 +4,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springbootserver.domain.todo.domain.Todo;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 
 import static com.springbootserver.domain.todo.domain.QTodo.*;
@@ -16,9 +19,12 @@ public class TodoQueryDslRepositoryImpl implements TodoQueryDslRepository {
     @Override
     public Map<String, List<Todo>> getTodoMapByIsDoneIsFalse() {
         Map<String , List<Todo>> todoMap = new HashMap<>();
+        LocalDate localDate = LocalDate.now().plusDays(1);
+        LocalTime of = LocalTime.of(0, 0);
+        LocalDateTime tomorrow = LocalDateTime.of(localDate, of);
         List<Todo> fetch = query
                 .selectFrom(todo)
-                .where(todo.isDone.eq(false))
+                .where(todo.isDone.eq(false).and(todo.dateTime.before(tomorrow)))
                 .fetch();
 
         fetch.forEach(t -> {
